462403627815f20b75802f3042cbc49b7cc211d2
Daniel Huebschmann <105037901+danielhdev@users.noreply.github.com>
2022-08-11 22:10:23 +0200

    Add files via upload

---
+++ Tailwind-basics-and navs/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontfamily: {
+        "poppins": ["Poppins", "sans-serif"],
+    },
+  },
+},
+}
